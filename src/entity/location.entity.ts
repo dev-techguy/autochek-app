c1d5609ef3333eb14180eea257297c31548faab8
dev-techguy <dev.techguy@gmail.com>
2021-06-12 16:08:46 +0300

    Minor updates

--- src/entity/location.entity.ts
+++ src/entity/location.entity.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { MaxLength } from 'class-validator';
+import { IsAlpha, IsNumber, IsUrl, MaxLength } from 'class-validator';
 import {
   BaseEntity,
   BeforeUpdate,
   Column,
   Entity,
+  IsNull,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -14,21 +15,25 @@ export class LocationEntity extends BaseEntity {
   id: number;
 
   @ApiProperty({ required: true })
+  @IsAlpha()
   @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
   @MaxLength(255)
   name: string;
 
   @ApiProperty({ required: true })
+  @IsNumber()
   @Column({ type: 'varchar', length: 255, nullable: false })
   @MaxLength(255)
   phoneNumber: string;
 
   @ApiProperty({ required: true })
+  @IsAlpha()
   @Column({ type: 'varchar', length: 255, nullable: false })
   @MaxLength(255)
   contactPerson: string;
 
   @ApiProperty({ required: false })
+  @IsUrl()
   @Column({ type: 'varchar', length: 255, nullable: true })
   @MaxLength(255)
   website: string;
